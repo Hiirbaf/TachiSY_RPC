07872f86e6266cc878e9aac3b497369ea0b830b1
Jery <jery99961@gmail.com>
2022-11-23 18:48:14 +0530

    Fixed critical bug + Day/Night Theme switch
    -> Version Bump | versionCode 10 | versionName "1.1.8-alpha"
    -> 'Things you can do' looks better in day theme now.
    -> Moved major '..wasChanged' codes into MainActivity.kt from Logic.kt
    -> Converted major 'if' statements to 'when'
    -> Added simple code for switching theme.
    -> Added feature to switch between day/night theme
    -> Created function- themeSwitch()
    -> added day/night theme option to menuItem
    -> Better visibility in action bar (Day theme)

--- app/src/main/java/com/jery/tachisy_rpc/utils/Logic.kt
+++ app/src/main/java/com/jery/tachisy_rpc/utils/Logic.kt
@@ -27,96 +27,8 @@ object Logic {
 
     private val chpName = MainActivity.chpName
     private val chpState = MainActivity.chpState
-    private val numType = MainActivity.numType
     lateinit var largeImage: String
     lateinit var smallImage: String
-
-    /**
-     * Set the correct chpName, chpState, chipIcons and numType when the chpName is changed
-     * @param activity The activity that calls this function
-     */
-    fun nameWasChanged(activity: Activity) {
-        if (chpName.getText().toString() == v_TachiyomiSy) {
-            chpName.text = v_LightNovel
-            chpState.text = v_MoonReader
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading_ln)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_moon_reader)
-            numType.value = 0
-        }
-        else if ((chpName.getText().toString() == v_LightNovel) || (chpName.getText().toString() == v_MoonReader)) {
-            chpName.text = v_Aniyomi
-            chpState.text = v_Anime
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_aniyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_watching)
-            numType.value = 2
-        }
-        else if ((chpName.getText().toString() == v_Aniyomi) || (chpName.getText().toString() == v_Anime)) {
-            chpName.text = v_Mangago
-            chpState.text = v_Manga
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_mangago)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-            numType.value = 0
-        }
-        else if (chpName.getText().toString() == v_Mangago) {
-            chpName.text = v_Webtoon
-            chpState.text = v_Reading
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_webtoon)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-            numType.value = 2
-        }
-        else if (chpName.getText().toString() == v_Webtoon) {
-            chpName.text = v_TachiyomiSy
-            chpState.text = v_Manga
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_tachiyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-            numType.value = 0
-        }
-        // This is a workaround for this weird bug I can't fix...
-        // When close app from recent apps and reopened, chpName can't be changed anymore.
-        // I believe it might be cuz the fancy names are not saved in the exact same format in sharedPrefs
-        else {
-            println("Wasn't able to switch template properly!!\nSwitching back to fallback data.")
-            chpName.text = v_TachiyomiSy
-            chpState.text = v_Manga
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_tachiyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-            numType.value = 0
-        }
-    }
-
-    /**
-     * Set the correct chpName, chpState and chipIcon when the chpState is changed
-     * @param activity The activity that calls this function
-     */
-    fun stateWasChanged(activity: Activity) {
-        if (chpState.getText().toString() == v_Manga) {
-            chpState.text = v_Manhwa
-        } else if (chpState.getText().toString() == v_Manhwa) {
-            chpState.text = v_Manga
-        }
-        else if (chpState.getText().toString() == v_LightNovel) {
-            chpName.text = v_LightNovel
-            chpState.text = v_MoonReader
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading_ln)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_moon_reader)
-        } else if (chpState.getText().toString() == v_MoonReader) {
-            chpName.text = v_MoonReader
-            chpState.text = v_LightNovel
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_moon_reader)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading_ln)
-        }
-        else if (chpState.getText().toString() == v_Aniyomi) {
-            chpName.text = v_Aniyomi
-            chpState.text = v_Anime
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_aniyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_watching)
-        } else if (chpState.getText().toString() == v_Anime) {
-            chpName.text = v_Anime
-            chpState.text = v_Aniyomi
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_watching)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_aniyomi)
-        }
-    }
 
     /**
      * Set the correct chipIcons when MainActivity's onCreate is triggered
@@ -124,44 +36,28 @@ object Logic {
      */
     fun restoreCorrectDataOnCreate(activity: Activity) {
         // load the right chipIcon when restoring lastState
-        if (chpName.getText().toString() == v_TachiyomiSy) {
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_tachiyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
+        when {
+            chpName.getText().toString() == v_TachiyomiSy -> {
+                chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_tachiyomi)
+                chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
+            }
+            chpName.getText().toString() == v_Mangago -> {
+                chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_mangago)
+                chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
+            }
+            chpName.getText().toString() == v_LightNovel || chpName.text == v_MoonReader -> {
+                chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_moon_reader)
+                chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading_ln)
+            }
+            chpName.getText().toString() == v_Aniyomi || chpName.getText().toString() == v_Anime -> {
+                chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_aniyomi)
+                chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_watching)
+            }
+            chpName.getText().toString() == v_Webtoon -> {
+                chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_webtoon)
+                chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
+            }
         }
-        else if (chpName.getText().toString() == v_Mangago) {
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_mangago)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-        }
-        else if ((chpName.getText().toString() == v_LightNovel) || (chpName.text == v_MoonReader)) {
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_moon_reader)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading_ln)
-        }
-        else if ((chpName.getText().toString() == v_Aniyomi) || (chpName.getText().toString() == v_Anime)) {
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_aniyomi)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_watching)
-        }
-        else if (chpName.getText().toString() == v_Webtoon) {
-            chpName.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_webtoon)
-            chpState.chipIcon = AppCompatResources.getDrawable(activity, R.drawable.ic_reading)
-        }
-    }
-
-    /**
-     * restore all details from sharedPrefs
-     * @param
-     */
-    fun restoreFromLastState() {
-        // set the saved edtDetails from sharedPrefs
-        if (chpName.text == v_TachiyomiSy)
-            MainActivity.edtDetails.setText(MainActivity.sharedPreferences.getString("keyDetails_tachi", ""))
-        else if ((chpName.text == v_LightNovel) || (chpName.text == v_MoonReader))
-            MainActivity.edtDetails.setText(MainActivity.sharedPreferences.getString("keyDetails_ln", ""))
-        else if ((chpName.text == v_Aniyomi) || (chpName.text == v_Anime))
-            MainActivity.edtDetails.setText(MainActivity.sharedPreferences.getString("keyDetails_anime", ""))
-        else if (chpName.text == v_Mangago)
-            MainActivity.edtDetails.setText(MainActivity.sharedPreferences.getString("keyDetails_mangago", ""))
-        else if (chpName.text == v_Webtoon)
-            MainActivity.edtDetails.setText(MainActivity.sharedPreferences.getString("keyDetails_webtoon", ""))
     }
 
     /**
@@ -173,16 +69,13 @@ object Logic {
         MainActivity.prefsEditor.putString("keyName", MainActivity.chpName.text.toString()).commit()
         MainActivity.prefsEditor.putString("keyState", MainActivity.chpState.text.toString()).commit()
 
-        if (MainActivity.chpName.text == v_TachiyomiSy)
-            MainActivity.prefsEditor.putString("keyDetails_tachi", MainActivity.edtDetails.text.toString()).commit()
-        else if ((MainActivity.chpName.text == v_LightNovel) || (MainActivity.chpName.text == v_MoonReader))
-            MainActivity.prefsEditor.putString("keyDetails_ln", MainActivity.edtDetails.text.toString()).commit()
-        else if ((MainActivity.chpName.text == v_Aniyomi) || (MainActivity.chpName.text == v_Anime))
-            MainActivity.prefsEditor.putString("keyDetails_anime", MainActivity.edtDetails.text.toString()).commit()
-        else if (MainActivity.chpName.text == v_Mangago)
-            MainActivity.prefsEditor.putString("keyDetails_mangago", MainActivity.edtDetails.text.toString()).commit()
-        else if (MainActivity.chpName.text == v_Webtoon)
-            MainActivity.prefsEditor.putString("keyDetails_webtoon", MainActivity.edtDetails.text.toString()).commit()
+        when (MainActivity.chpName.text) {
+            v_TachiyomiSy -> MainActivity.prefsEditor.putString("keyDetails_tachi", MainActivity.edtDetails.text.toString()).commit()
+            v_LightNovel, v_MoonReader -> MainActivity.prefsEditor.putString("keyDetails_ln", MainActivity.edtDetails.text.toString()).commit()
+            v_Aniyomi, v_Anime -> MainActivity.prefsEditor.putString("keyDetails_anime", MainActivity.edtDetails.text.toString()).commit()
+            v_Mangago -> MainActivity.prefsEditor.putString("keyDetails_mangago", MainActivity.edtDetails.text.toString()).commit()
+            v_Webtoon -> MainActivity.prefsEditor.putString("keyDetails_webtoon", MainActivity.edtDetails.text.toString()).commit()
+        }
         // save the current type and ch to sharedPrefs
         MainActivity.prefsEditor.putInt("keyType", MainActivity.numType.getValue()).commit()
         MainActivity.prefsEditor.putInt("keyCh", MainActivity.numChapter.getValue()).commit()
@@ -193,27 +86,27 @@ object Logic {
      * @param activity The activity that calls this function
      */
     fun loadRPCData(activity: MyService) {
-        if (chpName.getText().toString() == v_TachiyomiSy) {
-            largeImage = "attachments/961577469427736636/971135180322529310/unknown.png"
-            smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
-        }
-        else if ((chpName.getText().toString() == v_LightNovel) || (chpName.text == v_MoonReader)) {
-            largeImage = "attachments/949382602073210921/1031952390636707930/moon-reader-pro.png"
-            smallImage = "attachments/949382602073210921/994460304626962484/Reading-Icon.png"
-        }
-        else if ((chpName.getText().toString() == v_Aniyomi) || (chpName.getText().toString() == v_Anime))
-        {
-            largeImage = "attachments/949382602073210921/1002240570091122798/Aniyomi.png"
-            smallImage = "attachments/949382602073210921/1002240620569567404/watching-icon.png"
-        }
-        else if (chpName.getText().toString() == v_Mangago) {
-            largeImage = "attachments/949382602073210921/1034172617311133847/mangago.jpg"
-            smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
-        }
-        else if (chpName.getText().toString() == v_Webtoon)
-        {
-            largeImage = "attachments/1035528103017066618/1035529154600382515/Webtoon.png"
-            smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
+        when {
+            chpName.getText().toString() == v_TachiyomiSy -> {
+                largeImage = "attachments/961577469427736636/971135180322529310/unknown.png"
+                smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
+            }
+            chpName.getText().toString() == v_LightNovel || chpName.text == v_MoonReader -> {
+                largeImage = "attachments/949382602073210921/1031952390636707930/moon-reader-pro.png"
+                smallImage = "attachments/949382602073210921/994460304626962484/Reading-Icon.png"
+            }
+            chpName.getText().toString() == v_Aniyomi || chpName.getText().toString() == v_Anime -> {
+                largeImage = "attachments/949382602073210921/1002240570091122798/Aniyomi.png"
+                smallImage = "attachments/949382602073210921/1002240620569567404/watching-icon.png"
+            }
+            chpName.getText().toString() == v_Mangago -> {
+                largeImage = "attachments/949382602073210921/1034172617311133847/mangago.jpg"
+                smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
+            }
+            chpName.getText().toString() == v_Webtoon -> {
+                largeImage = "attachments/1035528103017066618/1035529154600382515/Webtoon.png"
+                smallImage = "attachments/949382602073210921/1001372717783711814/reading-icon.png"
+            }
         }
     }
 }
